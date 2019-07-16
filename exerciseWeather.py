7c571b4df7a01bf94994d052d8de287dcd0f36cf
Matthew Mackreth <MMackreth@spartaglobal.com>
2019-07-16 11:40:08 +0100

    Ammendments

--- exerciseWeather.py
+++ exerciseWeather.py
@@ -1,15 +1,12 @@
 # Takes input of weather from the user
-weather = input('What is the weather like?').lower()
+weather = input('What is the weather like? ').lower().strip()
 
 # Evaluates whether input matches any of the following in order
-if weather == 'rainy':
-    print('You should bring an umbrella')
-elif weather == 'sunny':
-    print('You should bring a sunhat')
-elif weather == 'snowy':
-    print('Do you want to build a snowman')
-elif weather == 'cloudy':
-    print('I would bring a jacket it might be cold')
+if 'rainy' in weather and 'stormy' in weather:
+    print('Bring a jacket and umbrella')
+elif 'rainy' in weather or 'foggy' in weather:
+    print('Bring an umbrella')
+elif 'sunny' in weather:
+    print('Wear a sunhat')
 else:
-    print("I've never heard of that weather before, sorry!")
-
+    print('Live your best life')
